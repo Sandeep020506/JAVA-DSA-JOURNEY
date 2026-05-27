ff0677dcf67a30494b67f243e5b15a77c78440ba
Sandeep Solanki <isanndeep01@gmail.com>
2026-05-27 14:17:51 +0530

    Learn Pattern Printing

---
+++ 08-PATTERNS-PRINTING/SquarePattern.java
@@ -0,0 +1,20 @@
+/*
+   Square Pattern
+   Concept:
+   - Nested Loops
+*/
+
+public class SquarePattern {
+
+    public static void main(String[] args) {
+
+        for (int row = 1; row <= 4; row++) {
+
+            for (int col = 1; col <= 4; col++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
